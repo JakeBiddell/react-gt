b7f7c1c9736d0e6df27fcba3e1a3e2d8b0d5324e
Jake Biddell <Jake.Biddell@UtilityPoint.co.uk>
2021-08-18 16:54:55 +0100

    fixed styles

---
+++ src/styles.ts
@@ -0,0 +1,26 @@
+const styles = `
+.__react-gt__dot {
+    cursor: pointer;
+    display: contents;
+    width: 10px;
+    height: 10px;
+    background-color: inherit;
+    border: 1px solid #757575;
+    border-radius: 50%;
+    margin-right: 7px;
+    overflow: hidden;
+}
+.__react-gt__dot:hover {
+    background-color: #757575;
+}
+.__react-gt__dot-selected {
+    cursor: default;
+    background-color: #00c19f;
+    border: 1px solid #00c19f;
+}
+.__react-gt__dot-selected:hover {
+    background-color: #00c19f;
+}
+`;
+
+export default styles;
