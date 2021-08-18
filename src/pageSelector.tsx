01230293563c3228134af61fe075a5d4b2aaa02c
Jake Biddell <Jake.Biddell@UtilityPoint.co.uk>
2021-08-18 15:11:34 +0100

    changed dot back

--- src/pageSelector.tsx
+++ src/pageSelector.tsx
@@ -13,19 +13,21 @@ const DotButton = ({ selected, onClick }: { selected: boolean; onClick: MouseEve
         }}
         onClick={onClick}
     >
-        <Hover style={{ backgroundColor: selected ? '#00c19f' : '#757575' }}>
-            <div
-                style={{
-                    width: '10px',
-                    height: '10px',
-                    backgroundColor: selected ? '#00c19f' : undefined,
-                    border: `1px solid ${selected ? '#00c19f' : '#757575'}`,
-                    borderRadius: '50%',
-                    marginRight: '7px',
-                    overflow: 'hidden',
-                }}
-            />
-        </Hover>
+        <div
+            style={{
+                width: '10px',
+                height: '10px',
+                backgroundColor: selected ? '#00c19f' : undefined,
+                border: `1px solid ${selected ? '#00c19f' : '#757575'}`,
+                borderRadius: '50%',
+                marginRight: '7px',
+                overflow: 'hidden',
+            }}
+        >
+            <Hover style={{ backgroundColor: selected ? '#00c19f' : '#757575' }}>
+                <div style={{ width: '10px', height: '10px' }} />
+            </Hover>
+        </div>
     </button>
 );
 
