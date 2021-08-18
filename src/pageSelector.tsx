219117502408a222da3da4942cdc501298ba0fe3
Jake Biddell <Jake.Biddell@UtilityPoint.co.uk>
2021-08-18 16:35:37 +0100

    changed style for dot

--- src/pageSelector.tsx
+++ src/pageSelector.tsx
@@ -8,21 +8,29 @@ const DotButton = ({ selected, onClick }: { selected: boolean; onClick: MouseEve
     return (
         <>
             <style>{`
-                __react-gt__dot {
-                    cursor: ${selected ? 'default' : 'pointer'};
-                    display: contents;
-                    width: 10px;
-                    height: 10px;
-                    backgroundColor: ${selected ? '#00c19f' : 'inherit'};
-                    border: 1px solid ${selected ? '#00c19f' : '#757575'};
-                    borderRadius: 50%;
-                    marginRight: 7px;
-                    overflow: hidden;
-                }
-                __react-gt__dot:hover {
-                    backgroundColor: ${selected ? '#00c19f' : '#757575'}
-                }
-            `}</style>
+__react-gt__dot {
+    cursor: pointer;
+    display: contents;
+    width: 10px;
+    height: 10px;
+    background-color: inherit;
+    border: 1px solid #757575;
+    border-radius: 50%;
+    margin-right: 7px;
+    overflow: hidden;
+}
+__react-gt__dot:hover {
+    background-color: #757575;
+}
+__react-gt__dot-selected {
+    cursor: default;
+    background-color: #00c19f;
+    border: 1px solid #00c19f;
+}
+__react-gt__dot-selected:hover {
+    background-color: #00c19f;
+}
+`}</style>
             <button type="button" className="__react-gt__dot" onClick={onClick} />
         </>
     );
