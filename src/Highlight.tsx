0d9a7c813aae459e5a19547c873f944f68dec75a
Jake Biddell <Jake.Biddell@UtilityPoint.co.uk>
2021-08-18 12:40:03 +0100

    fixed decontruction error

--- src/Highlight.tsx
+++ src/Highlight.tsx
@@ -7,7 +7,7 @@ const speed = '0.4';
 type ArrowProps = { x: number; y: number; rotation: number; scrollToElement: () => void };
 const Arrow = ({ x, y, rotation, scrollToElement }: ArrowProps) => {
     const ref = useRef(undefined as SVGPathElement);
-    const { height, width, top, bottom } = ref.current?.getBoundingClientRect();
+    const { height, width, top, bottom } = ref.current?.getBoundingClientRect() ?? {};
     return (
         <>
             <div
