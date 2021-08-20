977aef20ea0ea0ae99295e755611bba80aab0ba8
Jake Biddell <Jake.Biddell@UtilityPoint.co.uk>
2021-08-20 12:08:27 +0100

    removed arrow label if not arrow

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { DownArrow, LeftArrow, RightArrow, UpArrow } from './Icons';
 
-const Arrow = ({ direction }: { direction: 'up' | 'left' | 'down' | 'right' | null }) => (
-    <div className="__react-gt__arrow">
-        {(() => {
-            switch (direction) {
-                case 'up':
-                    return <UpArrow />;
-                case 'down':
-                    return <DownArrow />;
-                case 'right':
-                    return <RightArrow />;
-                case 'left':
-                    return <LeftArrow />;
-                default:
-                    return null;
-            }
-        })()}
-    </div>
-);
+const Arrow = ({ direction }: { direction: 'up' | 'left' | 'down' | 'right' | null }) =>
+    !direction ? null : (
+        <div className="__react-gt__arrow">
+            {(() => {
+                switch (direction) {
+                    case 'up':
+                        return <UpArrow />;
+                    case 'down':
+                        return <DownArrow />;
+                    case 'right':
+                        return <RightArrow />;
+                    case 'left':
+                        return <LeftArrow />;
+                    default:
+                        return null;
+                }
+            })()}
+        </div>
+    );
 
 export default Arrow;
